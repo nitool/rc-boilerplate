8e734e27e3d6a9e1149017daa1f4e218a0b91469
Sebastian Niecikowski <snitool@wp.pl>
2020-10-23 20:59:43 +0200

    fix composer building

--- _/docker/composer_installer.sh
+++ _/docker/composer_installer.sh
@@ -5,7 +5,7 @@ set -e
 printf -- "\n\n downloading composer \n"
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '8a6138e2a05a8c28539c9f0fb361159823655d7ad2deecb371b04a83966c61223adc522b0189079e3e9e277cd72b8897') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '$(wget -q -O - https://composer.github.io/installer.sig)') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
